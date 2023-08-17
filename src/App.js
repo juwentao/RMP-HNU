5554ca55caabfc5db59b83b17c01c9faefa86c8f
Wentao <102165248+juwentao@users.noreply.github.com>
2023-08-17 12:19:47 +0800

    更改标语

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          built by Tao@HNU with all the love in the world
         </p>
         <a
           className="App-link"
